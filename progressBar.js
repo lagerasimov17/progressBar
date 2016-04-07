6980d91092945419b847d8037f8ce78ebb1dfe9b
lagerasimov17 <lagerasimov17@TBSLTMBA860.local>
2016-04-07 18:34:28 -0500

    JS is finished at the bar technically works
    A working version of a not yet correctly stylized progress bar…

--- progressBar.js
+++ progressBar.js
@@ -1,16 +1,40 @@
-
-
 var blockNumber = 0;
 var blockNumberString = blockNumber.toString();
 
-function addClass() {
-    document.getElementById(blockNumberString).className += "answered";
+function updateBlockUp() {
     blockNumber = blockNumber + 1;
+    blockNumberString = blockNumber.toString();
 }
 
-function takeClass() {
-    "use strict";
+function updateBlockDown() {
+    blockNumber = blockNumber - 1;
+    blockNumberString = blockNumber.toString();
+}
+
+function addNextClass() {
+    console.log(blockNumber);
+    document.getElementById(blockNumberString).className += " " + "answered";
+}
+
+function removeCurrentClass() {
     document.getElementById(blockNumberString).className = document.getElementById(blockNumberString).className.replace 
     ( /(?:^|\s)answered(?!\S)/g , '' );
-    blockNumber = blockNumber - 1;
 }
+
+function incrementUp() {
+    if (blockNumber != 9) {
+        removeCurrentClass();
+        updateBlockUp();
+        addNextClass();
+    }
+}
+
+function incrementDown() {
+    if (blockNumber != 0) {
+        removeCurrentClass();
+        updateBlockDown();
+        addNextClass();
+    }
+}
+
+
